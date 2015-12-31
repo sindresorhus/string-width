b64f9d91d7c5ecaa51a3e5752d067b42a1aa7b25
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:27:49 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
-	t.assert(fn('abcde') === 5);
-	t.assert(fn('古池や') === 6);
-	t.assert(fn('あいうabc') === 9);
-	t.assert(fn('ノード.js') === 9);
-	t.assert(fn('你好') === 4);
-	t.assert(fn('안녕하세요') === 10);
-	t.assert(fn('A\ud83c\ude00BC') === 5, 'surrogate');
-	t.assert(fn('\u001b[31m\u001b[39m') === 0);
-	t.end();
+test(t => {
+	t.is(fn('abcde'), 5);
+	t.is(fn('古池や'), 6);
+	t.is(fn('あいうabc'), 9);
+	t.is(fn('ノード.js'), 9);
+	t.is(fn('你好'), 4);
+	t.is(fn('안녕하세요'), 10);
+	t.is(fn('A\ud83c\ude00BC'), 5, 'surrogate');
+	t.is(fn('\u001b[31m\u001b[39m'), 0);
 });
