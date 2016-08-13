d367d294556310009e1e2e62dd30420413134265
Sindre Sorhus <sindresorhus@gmail.com>
2016-08-13 21:58:16 +0200

    ignore control characters
    https://en.wikipedia.org/wiki/C0_and_C1_control_codes

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import fn from './';
 
-test(t => {
+test('main', t => {
 	t.is(fn('abcde'), 5);
 	t.is(fn('古池や'), 6);
 	t.is(fn('あいうabc'), 9);
@@ -10,4 +10,13 @@ test(t => {
 	t.is(fn('안녕하세요'), 10);
 	t.is(fn('A\ud83c\ude00BC'), 5, 'surrogate');
 	t.is(fn('\u001b[31m\u001b[39m'), 0);
+});
+
+test('ignores control characters', t => {
+	t.is(fn(String.fromCharCode(0)), 0);
+	t.is(fn(String.fromCharCode(31)), 0);
+	t.is(fn(String.fromCharCode(127)), 0);
+	t.is(fn(String.fromCharCode(134)), 0);
+	t.is(fn(String.fromCharCode(159)), 0);
+	t.is(fn('\u001b'), 0);
 });
