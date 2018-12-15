5b6b7ca6e764d3a3ad107d887edd24d0a17d8c03
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-15 15:08:38 +0100

    Require Node.js 6

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 import test from 'ava';
-import m from '.';
+import stringWidth from '.';
 
 test('main', t => {
-	t.is(m('abcde'), 5);
-	t.is(m('古池や'), 6);
-	t.is(m('あいうabc'), 9);
-	t.is(m('ノード.js'), 9);
-	t.is(m('你好'), 4);
-	t.is(m('안녕하세요'), 10);
-	t.is(m('A\uD83C\uDE00BC'), 5, 'surrogate');
-	t.is(m('\u001B[31m\u001B[39m'), 0);
-	t.is(m('\u{231A}'), 2, '⌚ default emoji presentation character (Emoji_Presentation)');
-	t.is(m('\u{2194}\u{FE0F}'), 2, '↔️ default text presentation character rendered as emoji');
-	t.is(m('\u{1F469}'), 2, '👩 emoji modifier base (Emoji_Modifier_Base)');
-	t.is(m('\u{1F469}\u{1F3FF}'), 2, '👩🏿 emoji modifier base followed by a modifier');
+	t.is(stringWidth('abcde'), 5);
+	t.is(stringWidth('古池や'), 6);
+	t.is(stringWidth('あいうabc'), 9);
+	t.is(stringWidth('ノード.js'), 9);
+	t.is(stringWidth('你好'), 4);
+	t.is(stringWidth('안녕하세요'), 10);
+	t.is(stringWidth('A\uD83C\uDE00BC'), 5, 'surrogate');
+	t.is(stringWidth('\u001B[31m\u001B[39m'), 0);
+	t.is(stringWidth('\u{231A}'), 2, '⌚ default emoji presentation character (Emoji_Presentation)');
+	t.is(stringWidth('\u{2194}\u{FE0F}'), 2, '↔️ default text presentation character rendered as emoji');
+	t.is(stringWidth('\u{1F469}'), 2, '👩 emoji modifier base (Emoji_Modifier_Base)');
+	t.is(stringWidth('\u{1F469}\u{1F3FF}'), 2, '👩🏿 emoji modifier base followed by a modifier');
 });
 
 test('ignores control characters', t => {
-	t.is(m(String.fromCharCode(0)), 0);
-	t.is(m(String.fromCharCode(31)), 0);
-	t.is(m(String.fromCharCode(127)), 0);
-	t.is(m(String.fromCharCode(134)), 0);
-	t.is(m(String.fromCharCode(159)), 0);
-	t.is(m('\u001B'), 0);
+	t.is(stringWidth(String.fromCharCode(0)), 0);
+	t.is(stringWidth(String.fromCharCode(31)), 0);
+	t.is(stringWidth(String.fromCharCode(127)), 0);
+	t.is(stringWidth(String.fromCharCode(134)), 0);
+	t.is(stringWidth(String.fromCharCode(159)), 0);
+	t.is(stringWidth('\u001B'), 0);
 });
 
 test('handles combining characters', t => {
-	t.is(m('x\u0300'), 1);
+	t.is(stringWidth('x\u0300'), 1);
 });
