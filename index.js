4e0d6d96d235cb2d3019ab678525ed99fc9296c7
fisker Cheung <lionkay@gmail.com>
2021-02-28 20:22:10 +0800

    Improve performance

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const stringWidth = string => {
 	if (typeof string !== 'string' || string.length === 0) {
 		return 0;
 	}
-
-	string = string.replace(emojiRegex(), '  ');
+	
+	string = stripAnsi(string);
 
 	if (string.length === 0) {
 		return 0;
 	}
 
-	string = stripAnsi(string);
+	string = string.replace(emojiRegex(), '  ');
 
 	let width = 0;
 
