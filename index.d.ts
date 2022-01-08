ffb6fe309a7ae5b0aece8fb867c66b4f53ba6706
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-01-08 17:19:56 +0900

    Add API doc to README.md

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export interface Options {
 	/**
-	Count [ambiguous width characters](https://www.unicode.org/reports/tr11/#Ambiguous) as having narrow width (count of 1) instead of wide (count of 2).
+	Count [ambiguous width characters](https://www.unicode.org/reports/tr11/#Ambiguous) as having narrow width (count of 1) instead of wide width (count of 2).
 
 	@default false
 	*/
