eade861ad0ffd265c1910efa8a29e29e4b4d52c4
dmail <dmaillard06@gmail.com>
2025-03-25 16:38:32 +0100

    Revert formatting done by prettier it fails the build

--- test.js
+++ test.js
@@ -2,19 +2,19 @@ import test from 'ava';
 import stringWidth from './index.js';
 
 test('main', t => {
-	t.is(stringWidth('⛣', { ambiguousIsNarrow: false }), 2);
+	t.is(stringWidth('⛣', {ambiguousIsNarrow: false}), 2);
 	t.is(stringWidth('abcde'), 5);
 	t.is(stringWidth('古池や'), 6);
 	t.is(stringWidth('あいうabc'), 9);
 	t.is(stringWidth('あいう★'), 7);
-	t.is(stringWidth('あいう★', { ambiguousIsNarrow: false }), 8);
+	t.is(stringWidth('あいう★', {ambiguousIsNarrow: false}), 8);
 	t.is(stringWidth('±'), 1);
 	t.is(stringWidth('ノード.js'), 9);
 	t.is(stringWidth('你好'), 4);
 	t.is(stringWidth('안녕하세요'), 10);
 	t.is(stringWidth('A\uD83C\uDE00BC'), 5, 'surrogate');
 	t.is(stringWidth('\u001B[31m\u001B[39m'), 0);
-	t.is(stringWidth('\u001B[31m\u001B[39m', { countAnsiEscapeCodes: true }), 8);
+	t.is(stringWidth('\u001B[31m\u001B[39m', {countAnsiEscapeCodes: true}), 8);
 	t.is(stringWidth('\u001B]8;;https://github.com\u0007Click\u001B]8;;\u0007'), 5);
 	t.is(stringWidth('\u{231A}'), 1, '⌚ default emoji presentation character (Emoji_Presentation)');
 	t.is(stringWidth('\u{2194}\u{FE0F}'), 2, '↔️ default text presentation character rendered as emoji');
@@ -23,7 +23,7 @@ test('main', t => {
 	t.is(stringWidth('\u{845B}\u{E0100}'), 2, 'Variation Selectors');
 	t.is(stringWidth('ปฏัก'), 3, 'Thai script');
 	t.is(stringWidth('_\u0E34'), 1, 'Thai script');
-	t.is(stringWidth('“', { ambiguousIsNarrow: false }), 2);
+	t.is(stringWidth('“', {ambiguousIsNarrow: false}), 2);
 });
 
 test('ignores control characters', t => {
